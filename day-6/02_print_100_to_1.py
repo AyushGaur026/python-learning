89bea313c7aa5e75ed1065e969ebabee3d331ac9
Ayush Kumar Gaur <ak5953051@gmail.com>
2026-07-23 04:31:49 -0700

    Complete Day 6: While Loop Basics

---
+++ day-6/02_print_100_to_1.py
@@ -0,0 +1,6 @@
+#Print numbers from 100 to 1 using a while loop.
+
+i = 100
+while i>=1:
+    print(i)
+    i-=1
---
+++ day-6/04_print_list.py
@@ -0,0 +1,8 @@
+#Using a while loop, print all elements of the following list:
+numbers = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+i = 0
+
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
