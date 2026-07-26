664570912daee7e6c79a796cb0aaf103a1eec261
Ayush Kumar Gaur <ak5953051@gmail.com>
2026-07-26 03:54:41 -0700

    Complete Python Day 9: File Handling

---
+++ day-9/01_create_file.py
@@ -0,0 +1,8 @@
+file = open("demo.txt", "w")
+
+file.write("Hello, Ayush!\n")
+file.write("Welcome to Python File Handling.")
+
+file.close()
+
+print("File created successfully.")
---
+++ day-9/05_append_file.py
@@ -0,0 +1,2 @@
+f = open ("demo.txt", "a")
+f.write("\ni am a full stack developer")
